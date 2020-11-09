5c8b112b6283118e854998f5401a44d9d515e765
fourtimes <jjino@protonmail.com>
2020-11-09 13:26:02 +0530

    aws windows added

---
+++ aws-windows/resource.tf
@@ -0,0 +1,43 @@
+resource "aws_key_pair" "mykey" {
+  key_name   = "mykey"
+  public_key = file(var.publickey)
+}
+
+resource "aws_instance" "winServer" {
+  ami                    = var.windowsAmi[var.region]
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.mykey.key_name
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  subnet_id              = aws_subnet.main-public-1.id
+  user_data              = <<EOF
+                    <powershell>
+                    net user ${var.instanceUsername} '${var.instancePassword}' /add /y
+                    net localgroup administrators ${var.instanceUsername} /add
+                    winrm quickconfig -q
+                    winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
+                    winrm set winrm/config '@{MaxTimeoutms="1800000"}'
+                    winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+                    winrm set winrm/config/service/auth '@{Basic="true"}'
+                    netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
+                    netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
+                    net stop winrm
+                    sc.exe config winrm start=auto
+                    net start winrm
+                    </powershell>
+                    EOF
+
+
+  provisioner "file" {
+    source      = "test.txt"
+    destination = "C:/test.txt"
+  }
+  connection {
+    host     = coalesce(self.public_ip, self.private_ip)
+    type     = "winrm"
+    timeout  = "10m"
+    user     = var.instanceUsername
+    password = var.instancePassword
+    https    = false
+    insecure = true
+  }
+}
---
+++ aws-windows/variable.tf
@@ -0,0 +1,45 @@
+variable "accessKey" {
+  default = "AKIAJC52CCCJTM46NDBA"
+}
+
+variable "secretKey" {
+  default = "pg27p+owdivuCC7jNw2DSisKaDUqpjLC4QTcCUcc"
+}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "windowsAmi" {
+  type = map(string)
+  default = {
+    eu-west-1 = "ami-0e2afec765bb99e9d"
+  }
+}
+
+variable "privateKey" {
+  default = "test"
+}
+
+variable "publickey" {
+  default = "test.pub"
+}
+
+variable "instanceUsername" {
+  default = "Terraform"
+}
+
+variable "instancePassword" {
+  default = "Password@123"
+}
+
+
+variable "ports" {
+  type = map(list(string))
+  default = {
+    "443"  = ["0.0.0.0/0"]
+    "5985" = ["0.0.0.0/0"]
+    "5986" = ["0.0.0.0/0"]
+    "3389" = ["0.0.0.0/0"]
+  }
+}
