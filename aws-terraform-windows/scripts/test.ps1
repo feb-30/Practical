a3c8ade5448fb21891b1c66dd5c8425045d60f65
fourtimes <jjino@protonmail.com>
2020-11-10 23:04:36 +0530

    script added

--- aws-terraform-windows/scripts/test.ps1
+++ aws-terraform-windows/scripts/test.ps1
@@ -1,1 +1,30 @@
 Write-Host "Hello, World!"
+
+$URL="https://da3hntz84uekx.cloudfront.net/QlikSense/13.95/0/_MSI/Qlik_Sense_setup.exe"
+Invoke-WebRequest $URL -OutFile c:\software\Qlik_Sense_setup.exe
+
+# Create user and password and added into administrator group
+
+$Username = "digit1"
+$Password = "pDigit@!@#@123"
+$group = "Administrators"
+
+$adsi = [ADSI]"WinNT://$env:COMPUTERNAME"
+$existing = $adsi.Children | where {$_.SchemaClassName -eq 'user' -and $_.Name -eq $Username }
+
+if ($existing -eq $null) {
+
+    Write-Host "Creating new local user $Username."
+    & NET USER $Username $Password /add /y /expires:never
+    
+    Write-Host "Adding local user $Username to $group."
+    & NET LOCALGROUP $group $Username /add
+
+}
+else {
+    Write-Host "Setting password for existing local user $Username."
+    $existing.SetPassword($Password)
+}
+
+Write-Host "Ensuring password for $Username never expires."
+& WMIC USERACCOUNT WHERE "Name='$Username'" SET PasswordExpires=FALSE
