71807ed608d404d3630d88d60705db284820645d
fourtimes <jjino@protonmail.com>
2020-11-10 23:16:49 +0530

    script added

--- aws-terraform-windows/scripts/test.ps1
+++ aws-terraform-windows/scripts/test.ps1
@@ -1,9 +1,5 @@
-Write-Host "Hello, World!"
-
 $URL="https://da3hntz84uekx.cloudfront.net/QlikSense/13.95/0/_MSI/Qlik_Sense_setup.exe"
 Invoke-WebRequest $URL -OutFile c:\software\Qlik_Sense_setup.exe
-
-# Create user and password and added into administrator group
 
 $Username = "digit1"
 $Password = "pDigit@!@#@123"
@@ -28,3 +24,20 @@ else {
 
 Write-Host "Ensuring password for $Username never expires."
 & WMIC USERACCOUNT WHERE "Name='$Username'" SET PasswordExpires=FALSE
+
+
+
+$content = Get-Content -Path 'c:\software\scripts\spec1_cfg.txt'
+$newContent = $content -replace 'foo', 'bar'
+$newContent | Set-Content -Path 'c:\software\scripts\spec1_cfg.txt'
+
+
+$filePath = 'C:\file.txt'
+$tempFilePath = "$env:TEMP\$($filePath | Split-Path -Leaf)"
+$find = 'foo'
+$replace = 'bar'
+
+(Get-Content -Path $filePath) -replace $find, $replace | Add-Content -Path $tempFilePath
+
+Remove-Item -Path $filePath
+Move-Item -Path $tempFilePath -Destination $filePath
