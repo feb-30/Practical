f710e87b31d98c125ef8a27fbe81e2e0edcb0552
Your Name <you@example.com>
2020-11-11 12:45:07 +0530

    added

--- aws-terraform-windows/scripts/test.ps1
+++ aws-terraform-windows/scripts/test.ps1
@@ -7,7 +7,7 @@ $ComputerName = $env:computername | Select-Object
 
 
 $Username = "digit1"
-$Password = "pDigit@!@#@123"
+$Password = "Digit@!@#@123"
 $group = "Administrators"
 
 $adsi = [ADSI]"WinNT://$env:COMPUTERNAME"
@@ -36,8 +36,17 @@ $content = Get-Content -Path 'c:\software\scripts\spec1_cfg.txt'
 $newContent = $content -replace '%computername%', $ComputerName
 $newContent | Set-Content -Path 'c:\software\scripts\spec1_cfg1.txt'
 
+$content = Get-Content -Path 'c:\software\scripts\spec1_cfg1.txt'
+$newContent = $content -replace 'localhost', $Ipaddress
+$newContent | Set-Content -Path 'c:\software\scripts\spec1_cfg.txt'
 
 
 New-Item 'c:\QlikSenseStorage' -ItemType Directory
 New-SMBShare –Name AdminUsedShares  –Path C:\QlikSenseStorage  –FullAccess 'Administrators'
 New-SMBShare –Name DigitUsedShares  –Path C:\QlikSenseStorage  –FullAccess 'digit1'
+
+
+
+
+
+Qlik_Sense_setup.exe -silent -log "c:\software\qliksense.log" accepteula=1 desktopshortcut=1 skipstartservices=1 installdir="%ProgramFiles%\Qlik\Sense" userwithdomain=%computername%\digit1 userpassword="Digit@!@#@123" dbpassword="Digit@!@#@123" hostname=%computername% sharedpersistenceconfig="c:\software\spec1_cfg.txt" databasedumpfile="c:\software"
